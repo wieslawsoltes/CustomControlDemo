99b209abc9e0592cbe1455839e252fd71653d5f0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-02-21 21:29:52 +0100

    Initial import

---
+++ CustomControlDemo/CustomCheckBox.axaml.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace Avalonia.NETCoreApp
+{
+    public class CustomCheckBox : TemplatedControl
+    {
+        public static readonly StyledProperty<bool> IsCheckedProperty = 
+            AvaloniaProperty.Register<CustomCheckBox, bool>("IsChecked");
+
+        public bool IsChecked
+        {
+            get => GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public CustomCheckBox()
+        {
+            AddHandler(PointerPressedEvent, Handler, RoutingStrategies.Tunnel);
+        }
+
+        private void Handler(object? sender, PointerPressedEventArgs e)
+        {
+            IsChecked = !IsChecked;
+        }
+    }
+}
